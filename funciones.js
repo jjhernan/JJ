fbadad50b9c2bacbb6f07890b27380196a4f6b7e
JJ <jjhdzh@gmail.com>
2016-07-06 21:48:03 -0500

    Create funciones.js

---
+++ funciones.js
@@ -0,0 +1,21 @@
+function Contento(){
+var contento = ["Hola","vientos","libertad","sonreir","empeatia"];
+}
+function Triste(){
+var triste= ["soledad","llorar","extrañar","añorar","olvidar"];
+}
+function Enamorado(){
+var enamorado= ["amor","corazon","vida","soñar","amar"];
+}
+function Odio (){
+var odio= ["fuck","muerete","pudrete","largate","shet"];
+}
+function Intenso (){
+var intenso= ["correr","brincar","gritar","golpear","satanizar"];
+}
+function Soledad(){
+var soledad= ["aislamiento","abandono","retiro","incomunicacion","separacion"];
+}
+function Angustia(){
+var angustia= ["intranquilidad","malestar","pesadumbre","ansiedad","desconsuelo"];
+}
